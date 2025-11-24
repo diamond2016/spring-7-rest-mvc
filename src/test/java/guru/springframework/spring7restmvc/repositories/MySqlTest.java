47fec23c8dae65647dd7d1c2a26d32b922d1700c
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-11-24 12:36:57 +0100

    simplified test with @ServiceConnection

--- src/test/java/guru/springframework/spring7restmvc/repositories/MySqlTest.java
+++ src/test/java/guru/springframework/spring7restmvc/repositories/MySqlTest.java
@@ -8,15 +8,16 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.DynamicPropertyRegistry;
+//import org.springframework.test.context.DynamicPropertySource;
+//import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import guru.springframework.spring7restmvc.entities.Beer;
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
 
 @Testcontainers
@@ -25,21 +26,23 @@ import javax.sql.DataSource;
 public class MySqlTest {
 
     @Container
+    @ServiceConnection
     static MySQLContainer<?> mySqlContainer = new MySQLContainer<>("mysql:8.0.26");
 
-    @Autowired
-    DataSource dataSource;
+    //@Autowired
+    //DataSource dataSource;
 
     @Autowired
     BeerRepository beerRepository;
         
-    @DynamicPropertySource  
-    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url",
-                () -> mySqlContainer.getJdbcUrl());
-        registry.add("spring.datasource.username", () -> mySqlContainer.getUsername());
-        registry.add("spring.datasource.password", () -> mySqlContainer.getPassword());
-    }
+    // commented out due to use of ServiceConnection
+    //@DynamicPropertySource  
+    //static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+    //    registry.add("spring.datasource.url",
+    //            () -> mySqlContainer.getJdbcUrl());
+    //    registry.add("spring.datasource.username", () -> mySqlContainer.getUsername());
+    //    registry.add("spring.datasource.password", () -> mySqlContainer.getPassword());
+    //}
         
     @Test
     void testListBeers() {
@@ -47,7 +50,7 @@ public class MySqlTest {
         listBeers.forEach(beer -> {
             System.out.println(beer.getBeerName());
         });
-        System.out.println("DataSource Class: " + dataSource.getClass().getName());
+        //System.out.println("DataSource Class: " + dataSource.getClass().getName());
         System.out.println("Repository: " + beerRepository.getClass().getName());
         assertThat(listBeers.size()).isGreaterThan(0);
     }
