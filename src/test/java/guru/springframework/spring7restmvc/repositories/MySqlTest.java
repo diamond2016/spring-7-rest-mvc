7a7dc6f402a746adcb6b9e30d773ffaab4fe32d9
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-11-24 09:32:36 +0100

    add DataSource dynamic in MYSqlTest

--- src/test/java/guru/springframework/spring7restmvc/repositories/MySqlTest.java
+++ src/test/java/guru/springframework/spring7restmvc/repositories/MySqlTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import guru.springframework.spring7restmvc.entities.Beer;
@@ -20,20 +24,31 @@ import javax.sql.DataSource;
 @ActiveProfiles("localmysql")
 public class MySqlTest {
 
-        @Autowired
-        DataSource dataSource;
+    @Container
+    static MySQLContainer<?> mySqlContainer = new MySQLContainer<>("mysql:8.0.26");
 
-        @Autowired
-        BeerRepository beerRepository;
+    @Autowired
+    DataSource dataSource;
 
-        @Test
-        void testListBeers() {
-            List<Beer> listBeers = beerRepository.findAll();
-            listBeers.forEach(beer -> {
-                System.out.println(beer.getBeerName());
-            });
-            System.out.println("DataSource Class: " + dataSource.getClass().getName());
-            System.out.println("Repository: " + beerRepository.getClass().getName());
-            assertThat(listBeers.size()).isGreaterThan(0);
-        }
+    @Autowired
+    BeerRepository beerRepository;
+        
+    @DynamicPropertySource  
+    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url",
+                () -> mySqlContainer.getJdbcUrl());
+        registry.add("spring.datasource.username", () -> mySqlContainer.getUsername());
+        registry.add("spring.datasource.password", () -> mySqlContainer.getPassword());
+    }
+        
+    @Test
+    void testListBeers() {
+        List<Beer> listBeers = beerRepository.findAll();
+        listBeers.forEach(beer -> {
+            System.out.println(beer.getBeerName());
+        });
+        System.out.println("DataSource Class: " + dataSource.getClass().getName());
+        System.out.println("Repository: " + beerRepository.getClass().getName());
+        assertThat(listBeers.size()).isGreaterThan(0);
+    }
 }
