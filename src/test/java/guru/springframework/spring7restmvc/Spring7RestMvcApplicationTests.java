41d0e696db0d536991c760bb532d733dd57101e9
Riccardo Corsi <riccardogcorsi@gmail.com>
2026-01-20 18:17:00 +0100

    test: use Testcontainers MySQL for localmysql profile in Spring7RestMvcApplicationTests

--- src/test/java/guru/springframework/spring7restmvc/Spring7RestMvcApplicationTests.java
+++ src/test/java/guru/springframework/spring7restmvc/Spring7RestMvcApplicationTests.java
@@ -3,10 +3,30 @@ package guru.springframework.spring7restmvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("localmysql")
+@Testcontainers
 class Spring7RestMvcApplicationTests {
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.33")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+    }
 
     @Test
     void contextLoads() {
